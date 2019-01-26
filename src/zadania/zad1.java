58f0d9deb24f88ba9e83037dcd07c7ee00eabee3
krzychu <ojebalem.cale@gmail.com>
2019-01-26 13:41:43 +0100

    zad 1 gotowe //jak jestes ambitny to trzasnij tam potem test

---
+++ src/zadania/zad1.java
@@ -0,0 +1,34 @@
+package zadania;
+//1. Stwórz tablicę intów i wypełnij ją wartościami. Sprawdź czy elementy
+// w tablicy są posortowane niemalejąco. ROSNOCO KURWA Zwróć true, jeżeli prawda, wpp. false.
+public class zad1
+{
+    public static void main(String[] args)
+    {
+        int tablica[] = new int[5];
+        tablica[0] = 5;
+        tablica[1] = 4;
+        tablica[2] = 5;
+        tablica[3] = 5;
+        tablica[4] = 5;
+        for(int i = 0; i < 5; i++)
+        {
+            System.out.println(tablica[i]);     //dziala
+        }
+
+        System.out.println(czyMalejaca(tablica));
+    }
+
+    private static boolean czyMalejaca(int tab[])
+    {
+        int tmp = tab[0];   //jakas w chuj mala wartosc
+        for(int i = 0; i < tab.length; i++)
+        {
+            if(tmp>tab[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
